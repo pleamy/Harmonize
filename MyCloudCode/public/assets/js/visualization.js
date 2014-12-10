91e79ac0c4d965e22a053341e26191be08e6e39e
Amy Le <pleamy13@gmail.com>
2014-12-10 13:33:34 -0500

    force nodes usdate

--- MyCloudCode/public/assets/js/visualization.js
+++ MyCloudCode/public/assets/js/visualization.js
@@ -1,9 +1,7 @@
 var genreList = [];
-var Map = {};
 var mapSize = 0;
 var d3_genre = [];
 var playlist_id;
-
 var d3Map = d3.map();
 var trackFields = { fields:'offset,next,items(track(name,artists))' };
 var trackFields_2 = {};
@@ -19,7 +17,7 @@ function showTracks(tracks) {
     var force = d3.layout.force().gravity(0.13)
   								.charge(-30)
   								.size([600, 600]);
-	var	nodes = force.nodes(), centers = [];
+	
     // console.log('d3 entries', entries);
     // console.log('show tracks', tracks);
     
@@ -51,35 +49,33 @@ function showTracks(tracks) {
 				
 				if ((d3Map.get(gen) == undefined) || (!d3Map.has(gen))) {
 					d3Map.set(gen, 1);
-					nodes.push({
-		            	genre: gen, r: d3Map.get(gen), 
-		            	x: 800 / 2 + offset(), 
-		            	y: 800 / 2 + offset(), 
-		            	color: d3.rgb(245, 135, 54) 
-	           		});
+					
 				} else {
 					var mapVal = d3Map.get(gen);
 					mapVal++;
 					d3Map.remove(gen);
 					d3Map.set(gen, mapVal);
-					for (n in nodes) {
-						if(nodes[n].genre == gen) {
-							nodes[n].r = mapVal;
-							console.log("hey");
-							// nodes.push(nodes[n]);
-						}
-					}
+					
+						
+					
 				}
 
-                if (Map[gen] == undefined) {
-                    Map[gen] = 1;
-                    radius = Map[gen];
-                    genres[mapSize] = gen;
-                    mapSize++;
-                } else {
-                    Map[gen]++;
-                    radius = Map[gen];
-                } 
+				// for (n in nodes) {
+				// 	if(nodes[n].genre == gen) {
+				// 			// nodes.remove();
+				// 		nodes.push ({
+				// 				genre: gen, r: d3Map.get(gen), 
+				//             	x: 800 / 2 + offset(), 
+				//             	y: 800 / 2 + offset(), 
+				//             	color: d3.rgb(245, 135, 54) 
+				// 			});
+				// 			nodes[n].r = mapVal;
+				// 			console.log(nodes[n].r);
+				// 			console.log("hey");
+				// 			// nodes.push(nodes[n]);
+				// 	}
+				// }
+
 			});
 
 			
@@ -89,8 +85,19 @@ function showTracks(tracks) {
 			function offset() {
 			     return Math.random() * 100;
 		    }
-
-
+		    var nodes = null;
+		    var	nodes = force.nodes(), centers = [];
+		    for (k in keys) {
+		    	var g = keys[k];
+		    	var gName = d3Map.get(g);
+		    	nodes.push({
+		            	genre: g, r: gName, 
+		            	x: 800 / 2 + offset(), 
+		            	y: 800 / 2 + offset(), 
+		            	color: d3.rgb(245, 135, 54) 
+	           		});
+		    }
+		    
 
 			svg.selectAll("circle")
 				.data(nodes).enter()
@@ -101,7 +108,10 @@ function showTracks(tracks) {
 				.attr("fill", function(d){ return d.color; })
 				.attr("r", 1e-6).transition()
 				.attr("r", function(d) { 
-					console.log(d3Map.get(d.genre));
+					// console.log(d.genre);
+					// console.log(d3Map.values());
+					// console.log(d3Map.get(d.genre));
+					// d3Map.get(d.genre)
 					return d3Map.get(d.genre); 
 				})
 				
